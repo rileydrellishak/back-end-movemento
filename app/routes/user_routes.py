f551fd3b7af939a03ac274239fee4d9be98761cb
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-20 12:02:28 -0500

    get entries for specific user

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from app.models.user import User
+from app.models.journal_entry import JournalEntry
 from app.db import db
 from app.routes.route_utilities import create_model, get_models_with_filters, validate_model
 
@@ -18,3 +19,16 @@ def get_user_by_id(id):
 def create_user():
     user_data = request.get_json()
     return create_model(User, user_data)
+
+@bp.get('/<id>/entries')
+def get_entries_for_user(id):
+    user = validate_model(User, id)
+    response = user.to_dict()
+    response['journal_entries'] = [entry.to_dict() for entry in user.journal_entries]
+    return response, 200
+
+# @bp.post('/<id>/entries')
+# def post_entry_for_user(id):
+#     user = validate_model(User, id)
+#     request_body = request.get_json()
+    
