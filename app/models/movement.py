a2b1153133ba14f143ad9effee1b3635d942c4e6
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-20 10:29:49 -0500

    initial movement model created

--- app/models/movement.py
+++ app/models/movement.py
@@ -1,8 +1,18 @@
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import String, Boolean
+from sqlalchemy import String, Boolean, Integer
 from ..db import db
 
 # movements have id, name, slug, category, is_outdoor
 
 class Movement(db.Model):
     __tablename__ = 'movements'
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True)
+    
+    name: Mapped[str] = mapped_column(String(30))
+    slug: Mapped[str] = mapped_column(String(30))
+    category: Mapped[str] = mapped_column(String(30))
+    is_outdoor: Mapped[bool] = mapped_column(Boolean)
