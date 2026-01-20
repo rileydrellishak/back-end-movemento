79ed3866d2cd08e6235f6b7c7b86f7f84fd858e9
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-20 10:56:58 -0500

    initialize journal entry model

--- app/models/journal_entry.py
+++ app/models/journal_entry.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, String, DateTime
+from sqlalchemy import ForeignKey, String, DateTime, Integer
+from typing import Optional
 from app.db import db
 from datetime import datetime
 
@@ -7,3 +8,26 @@ from datetime import datetime
 
 class JournalEntry(db.Model):
     __tablename__ = 'journal_entry'
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    # movements: Mapped[list['Movement']] = 
+    # moods_before: Mapped[list['Mood']] = 
+    # moods_after: Mapped[list['Mood']] =
+
+    reflection: Mapped[str] = mapped_column(String(511))
+    
+    # user_id: Mapped['User'] = 
+    
+    img_path: Mapped[Optional[str]] = mapped_column(
+        nullable=True
+    )
+    
+    created_at: Mapped[Optional[datetime]] = mapped_column(
+        DateTime,
+
+    )
