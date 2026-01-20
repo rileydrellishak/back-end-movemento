4cea760c3b7d0cc00e85787b5cb6488a7f443a9d
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-20 10:47:50 -0500

    initialize mood model

--- app/models/mood.py
+++ app/models/mood.py
@@ -1,8 +1,19 @@
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, String, Integer
-from ..db import db
+from app.db import db
 
 # moods have id, name, slug, valence, energy
 
 class Mood(db.Model):
     __tablename__ = 'mood'
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    name: Mapped[str] = mapped_column(String(30))
+    slug: Mapped[str] = mapped_column(String(30))
+    valence: Mapped[str] = mapped_column(String(30))
+    energy: Mapped[str] = mapped_column(String(30))
