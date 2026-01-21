a9485ffec7f8603572661f34e6ee8f77c2d552d0
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-21 13:36:08 -0500

    tests to and from dict method for user, still need to write when user_data is missing name and has extra key-value pairs

--- tests/models/test_user_model.py
+++ tests/models/test_user_model.py
@@ -4,20 +4,16 @@ import pytest
 
 def test_user_to_dict_method():
     user = User(
-        id=1,
-        name='Riley',
-        journal_entries=[]
+        name='Riley'
     )
 
     user_dict = user.to_dict()
-    assert type(user_dict) == dict
     assert user_dict == {
-        'id': 1,
+        'id': None,
         'name': 'Riley',
         'journal_entries': []
     }
 
-@pytest.mark.skip(reason='things are silly')
 def test_user_from_dict_method():
     user_data = {
         'name': 'Riley'
@@ -26,3 +22,4 @@ def test_user_from_dict_method():
 
     assert user.name == 'Riley'
     assert user.journal_entries ==[]
+    assert user.id is None
