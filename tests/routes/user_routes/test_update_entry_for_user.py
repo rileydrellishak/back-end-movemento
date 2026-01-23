e5bfb6a1b91f5b28b3947100149ffa14609247a1
Riley Drellishak <rileydrellishak@gmail.com>
2026-01-23 10:48:54 -0500

    writes tests for updating a journal entry

--- tests/routes/user_routes/test_update_entry_for_user.py
+++ tests/routes/user_routes/test_update_entry_for_user.py
@@ -1,9 +1,10 @@
 from app.models.journal_entry import JournalEntry
 from app.db import db
 
-def test_update_journal_entry(client, user_one, journal_entry_one):
+def test_update_journal_entry(client, user_one, journal_entry_one, movement_one, movement_two, movement_three, movement_four):
     entry_data = {
-        'reflection': 'I am updating my reflection!'
+        'reflection': 'I am updating my reflection!',
+        'movements': [1, 3]
     }
     
     response = client.patch('/users/1/entries/1', json=entry_data)
@@ -14,10 +15,12 @@ def test_update_journal_entry(client, user_one, journal_entry_one):
 
     assert response.status_code == 204
     assert updated_entry.reflection == 'I am updating my reflection!'
+    assert updated_entry.movements == [1, 3]
 
-def test_update_journal_entry_je_invalid_id(client, user_one, journal_entry_one):
+def test_update_journal_entry_je_invalid_id(client, user_one, journal_entry_one, movement_one, movement_two, movement_three, movement_four):
     entry_data = {
-        'reflection': 'I am updating my reflection!'
+        'reflection': 'I am updating my reflection!',
+        'movements': [1, 3]
     }
     
     response = client.patch('/users/1/entries/one', json=entry_data)
@@ -28,13 +31,43 @@ def test_update_journal_entry_je_invalid_id(client, user_one, journal_entry_one)
         'message': 'Id one invalid. Ids must be integers.'
     }
     assert journal_entry_one.reflection != 'I am updating my reflection!'
+    assert journal_entry_one.movements != [1, 2]
 
-def test_update_journal_entry_entry_not_found(client, user_one, journal_entry_one):
+def test_update_journal_entry_entry_not_found(client, user_one, journal_entry_one, movement_one, movement_two, movement_three, movement_four):
     entry_data = {
-        'reflection': 'I am updating my reflection!'
+        'reflection': 'I am updating my reflection!',
+        'movements': [1, 3]
     }
     
     response = client.patch('/users/1/entries/5', json=entry_data)
     response_body = response.get_json()
 
     assert response.status_code == 404
+    assert response_body == {
+        'message': 'JournalEntry with id 5 not found.'
+    }
+    assert journal_entry_one.reflection != 'I am updating my reflection!'
+    assert journal_entry_one.movements != [1, 3]
+
+def test_update_journal_entry_missing_appropriate_keys(client, user_one, journal_entry_one, movement_one, movement_two, movement_three, movement_four):
+    entry_data = {
+        'my feelings': 'a made up key value pair',
+        'my thoughts': [1, 2, 3],
+        'reflection': 'new reflection'
+    }
+    
+    response = client.patch('/users/1/entries/1', json=entry_data)
+    response_body = response.get_json()
+
+    assert response.status_code == 204
+    assert not hasattr(journal_entry_one, 'my feelings')
+    assert not hasattr(journal_entry_one, 'my thoughts')
+    assert journal_entry_one.reflection == 'new reflection'
+
+def test_update_journal_entry_nothing_to_update(client, user_one, journal_entry_one):
+    entry_data = {}
+
+    response = client.patch('/users/1/entries/1', json=entry_data)
+    response_body = response.get_json()
+
+    assert response.status_code == 204
